f2959a04494041be70c6e5697d5c0684c87b4376
camilajaviera <cjmunozn@hotmail.com>
2024-12-16 13:54:20 -0300

    feat(prediction): import libraries and create a function to run kaggle, finally we create the dataframe so we can use the dataset that we add in the previous step.

---
+++ prediction.py
@@ -0,0 +1,14 @@
+#Import necessary libraries
+
+import pandas as pd #For creating and manipulating DataFrames and Series
+import curses as cr #Create text-based user interfaces (TUIs) in the terminal
+from kaggle_connect import kaggle_connect as kc #Custom function to fetch the dataset using Kaggle API
+
+#Wrapper function to run Kaggle connect using curses.
+def run_kaggle():
+    return cr.wrapper(kc) #kc is passed as a function, not as a pre-executed call.
+
+boston = run_kaggle()
+data = pd.DataFrame(boston)
+
+print(data)
