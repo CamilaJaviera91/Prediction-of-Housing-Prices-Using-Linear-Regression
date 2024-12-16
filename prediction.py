dacd4634383a3a98708ed31f810b37f1afdb8b9f
camilajaviera <cjmunozn@hotmail.com>
2024-12-16 19:07:29 -0300

    feat(prediction): add new libraries.

--- prediction.py
+++ prediction.py
@@ -2,6 +2,11 @@
 
 import pandas as pd #For creating and manipulating DataFrames and Series
 import curses as cr #Create text-based user interfaces (TUIs) in the terminal
+from sklearn.model_selection import train_test_split as ts #Divides data into training and testing sets in a random and stratified way (if needed).
+from sklearn.linear_model import LinearRegression as lr #Fits a linear model to data by finding the line (or hyperplane) that minimizes the sum of squared differences between predicted and actual values.
+from sklearn.metrics import mean_squared_error as mse #Measures the average squared difference between predicted and actual values.
+from sklearn.metrics import mean_absolute_error as mae #Measures the average absolute difference between predicted and actual values.
+import numpy as np #Handles arrays, performs linear algebra, matrix computations, random number generation, and other mathematical operations.
 from kaggle_connect import kaggle_connect as kc #Custom function to fetch the dataset using Kaggle API
 
 #Wrapper function to run Kaggle connect using curses.
