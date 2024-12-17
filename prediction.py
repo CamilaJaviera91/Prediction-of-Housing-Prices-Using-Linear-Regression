29c1f27d413f205d988d020be3f2d4a2b656736e
camilajaviera <cjmunozn@hotmail.com>
2024-12-17 13:27:16 -0300

    feat(linear regression): add matplotlib.pyplot so we can create graphs.

--- prediction.py
+++ prediction.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LinearRegression as lr #Fits a linear model to 
 from sklearn.metrics import mean_squared_error as mse #Measures the average squared difference between predicted and actual values.
 from sklearn.metrics import mean_absolute_error as mae #Measures the average absolute difference between predicted and actual values.
 import numpy as np #Handles arrays, performs linear algebra, matrix computations, random number generation, and other mathematical operations.
+import matplotlib.pyplot as plt #Used for creating static, interactive, and dynamic visualizations in Python.
 from kaggle_connect import kaggle_connect as kc #Custom function to fetch the dataset using Kaggle API
 
 #Wrapper function to run Kaggle connect using curses.
